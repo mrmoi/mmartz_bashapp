886a633caddc93ed0853f20ae6bf5c0139adabd1
mrmoi <mlmart16@asu.edu>
2018-09-13 22:52:00 -0700

    commit

--- executor.sh
+++ executor.sh
@@ -3,6 +3,6 @@
 # Modify nametool.sh 
 echo $(date)  >> nametool.sh
 
-/usr/bin/git add .
-/usr/bin/git commit -m "commit"
-/usr/bin/git push origin master
+git add .
+git commit -m "commit"
+gitpush origin master
